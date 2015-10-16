4b259bd72ffc28d9134a44195fad1f7d431ce98c
MuraliKrishnaReddy P <mp00356554@techmahindra.com>
2015-10-16 19:07:14 +0530

    checking compilation error

--- demo1/src/main/java/com/cisco1/Starting.java
+++ demo1/src/main/java/com/cisco1/Starting.java
@@ -5,7 +5,7 @@ public class Starting {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hai all");asfdef
+		System.out.println("Hai all");
 
 	}
 
