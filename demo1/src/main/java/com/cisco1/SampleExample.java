f27cb2bc967ab4708673da56187313c053032fd7
MuraliKrishnaReddy P <mp00356554@techmahindra.com>
2015-11-16 10:49:38 +0530

    Added test file to run test cases

---
+++ demo1/src/main/java/com/cisco1/SampleExample.java
@@ -0,0 +1,25 @@
+package com.cisco1;
+
+import java.util.List;
+
+import java.util.ArrayList;
+
+public class SampleExample {
+	
+   private List<Integer> integers = null;
+   public SampleExample() {
+      integers = new ArrayList<Integer> ();
+   }
+   public void addInteger(int num) {
+      integers.add(num);
+
+   }
+   public int getSize() {
+
+      return integers.size();
+
+   }
+
+}
+
+
---
+++ demo1/src/test/java/com/cisco1/SampleExampleTest.java
@@ -0,0 +1,22 @@
+package com.cisco1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleExampleTest {
+	
+	@Test
+	   public void test() {
+	      SampleExample ex1 = new SampleExample();
+	      ex1.addInteger(10);
+	      ex1.addInteger(100);
+
+	      SampleExample ex2 = new SampleExample();
+	      ex2.addInteger(10);
+	      ex2.addInteger(100);
+	      ex2.addInteger(1000);
+
+	      Assert.assertNotEquals(ex1.getSize(), ex2.getSize());
+	   }
+
+}
